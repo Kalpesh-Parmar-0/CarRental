4ccd01893052c6bad6ac600000ea860fba6cac94
Kalpesh-Parmar-0 <kalpesh2112004@gmail.com>
2025-06-30 20:00:02 +0530

    Added testimonial section

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import Title from './Title'
+import { assets } from '../assets/assets';
 
 const Testimonial = () => {
+
+    const testimonials = [
+        {
+            name: "Emma Rodriguez",
+            location: "Barcelona, Spain", 
+            image: assets.testimonial_image_1,
+            testimonials: "I have rented cars from various componies, but the experience with CarRental was excellent." 
+        },
+        {
+            name: "Jhon Whick",
+            location: "New York, USA", 
+            image: assets.testimonial_image_2,
+            testimonials: "CarRental made my trip so much easier. The car was delivered right to my door, and customer service was fentastic." 
+        },
+        {
+            name: "Jonash Khanval",
+            location: "Sydney, Spain", 
+            image: assets.testimonial_image_1,
+            testimonials: "Customer service was not that good and car that given to me was not good as it was in picture." 
+        }
+    ];
+
   return (
-    <div>
-      
-    </div>
+    <div className="py-28 px-6 md:px-16 lg:px-24 xl:px-44">
+            
+            <Title title="What Our Customers Say" subTitle="Discover why discerning travelers choose StayVenture for their luxury accommodation around word."/>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
+                {testimonials.map((testimonial, index) => (
+                    <div key={index} className="bg-white p-6 rounded-xl shadow-lg hover:translate-y-1 transition-all duration-500">
+                        <div className="flex items-center gap-3">
+                            <img className="w-12 h-12 rounded-full" src={testimonial.image} alt={testimonial.name} />
+                            <div>
+                                <p className="text-xl">{testimonial.name}</p>
+                                <p className="text-gray-500">{testimonial.location}</p>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-1 mt-4">
+                            {Array(5).fill(0).map((_, index) => (
+                                <img key={index} src={assets.star_icon} alt="star" />
+                            ))}
+                        </div>
+                        <p className="text-gray-500 max-w-90 mt-4 font-light">"{testimonial.testimonials}"</p>
+                    </div>
+                ))}
+            </div>
+        </div>
   )
 }
 
