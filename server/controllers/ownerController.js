ab133b2cf517189f754758b8f348be02d3821cfc
Kalpesh-Parmar-0 <kalpesh2112004@gmail.com>
2025-07-04 10:42:15 +0530

    created model for car and added controller to list car in ownerController

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,10 +1,23 @@
 import User from "../models/User.js";
 
+// api to change role of user
 export const changeRoleToOwner = async (req, res) => {
     try {
         const {_id} = req.user;
         await User.findByIdAndUpdate(_id, {role: "owner"})
         res.json({success:true, message: "Now you can list cars"})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false, message: error.message})
+    }
+}
+
+// api to list car
+export const addCar = async (req, res) => {
+    try {
+        const {_id} = req.user;
+        let car = JSON.parse(req.body.carData);
+        const imageFile = req.file;
     } catch (error) {
         console.log(error.message)
         res.json({success:false, message: error.message})
