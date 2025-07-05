758ed23909eb99252b45788d0b257efc8ab90b33
Kalpesh-Parmar-0 <kalpesh2112004@gmail.com>
2025-07-05 10:39:37 +0530

    imagekit configuration done and create controller for listing car

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,4 +1,7 @@
+import imagekit from "../configs/imageKit.js";
 import User from "../models/User.js";
+import Car from "../models/Car.js"
+import fs from "fs";
 
 // api to change role of user
 export const changeRoleToOwner = async (req, res) => {
@@ -18,7 +21,43 @@ export const addCar = async (req, res) => {
         const {_id} = req.user;
         let car = JSON.parse(req.body.carData);
         const imageFile = req.file;
+
+        // upload image to imagekit
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname, 
+            folder:'/cars'
+        })
+
+        // optimize through image url transformation
+        var optimizedImageUrl = imagekit.url({
+            path : response.filePath,
+            transformation : [
+                {width:'1280'}, // width resize
+                {quality:'auto'}, // auto compression
+                {format: 'webp'} // convert to modern format
+            ]
+        });
+
+        const image = optimizedImageUrl;
+        await Car.create({...car, owner: _id, image})
+
+        res.json({success:true, message: "Car added"})
+
     } catch (error) {
+        console.log(error.message)
+        res.json({success:false, message: error.message})
+    }
+}
+
+// api to list owner car
+export const getOwnerCar = async (req, res) =>{
+    try{
+        const {_id} = req.user;
+        const cars = await Car.find({owner: _id})
+        res.json({success:true, cars})
+    } catch (error){
         console.log(error.message)
         res.json({success:false, message: error.message})
     }
