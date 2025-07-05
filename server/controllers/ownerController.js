7f69796861bc81f6f62a35637b1e6611cc2ab07e
Kalpesh-Parmar-0 <kalpesh2112004@gmail.com>
2025-07-05 15:54:46 +0530

    Controllers like toggle car avalability, delete car done and routes added for them

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -52,11 +52,70 @@ export const addCar = async (req, res) => {
 }
 
 // api to list owner car
-export const getOwnerCar = async (req, res) =>{
+export const getOwnerCars = async (req, res) =>{
     try{
         const {_id} = req.user;
         const cars = await Car.find({owner: _id})
         res.json({success:true, cars})
+    } catch (error){
+        console.log(error.message)
+        res.json({success:false, message: error.message})
+    }
+}
+
+// api to toggle car availability
+export const toggleCarAvailability = async (req, res) => {
+    try{
+        const {_id} = req.user;
+        const {carId} = req.body
+        const car = await Car.findById(carId)
+
+        // checking if car belongs to the user
+        if(car.owner.toString() !== _id.toString()){
+            return res.json({success:false, message: "Unauthorized"})
+        }
+
+        car.isAvaliable = !car.isAvaliable;
+        await car.save()
+
+        res.json({success:true, message: "Availability toggeled"})
+    } catch (error){
+        console.log(error.message)
+        res.json({success:false, message: error.message})
+    }
+}
+
+// api to delete a car
+export const deleteCar = async (req, res) => {
+    try{
+        const {_id} = req.user;
+        const {carId} = req.body
+        const car = await Car.findById(carId)
+
+        // checking if car belongs to the user
+        if(car.owner.toString() !== _id.toString()){
+            return res.json({success:false, message: "Unauthorized"})
+        }
+
+        car.owner = null;
+        car.isAvaliable = false;
+        await car.save()
+
+        res.json({success:true, message: "Car removed"})
+    } catch (error){
+        console.log(error.message)
+        res.json({success:false, message: error.message})
+    }
+}
+
+// api to get dashbord data
+export const getDashbordData = async (req, res) => {
+    try{
+        const {_id, role} = req.user;
+        if(role !== 'owner') {
+            return res.json({success:false, message: "Unauthorized"})
+        }
+        const cars = await Car.find(_id)
     } catch (error){
         console.log(error.message)
         res.json({success:false, message: error.message})
