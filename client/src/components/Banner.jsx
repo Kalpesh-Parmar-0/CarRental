34a5f58c22fbfc77e9795beeabd1705e6423f45a
Kalpesh <kalpesh2112004@gmail.com>
2026-04-13 20:20:16 +0530

    Banner updated so it gives different content to user and owner

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { use } from 'react'
 import { assets } from '../assets/assets'
 import {motion} from 'motion/react'
+import { useAppContext } from '../context/AppContext'
 
 const Banner = () => {
+  const {isOwner} = useAppContext()
   return (
     <motion.div
     initial = {{opacity: 0, y: 50}}
@@ -10,7 +12,19 @@ const Banner = () => {
     transition={{duration: 0.6}}
     className='flex flex-col md:flex-row md:items-start items-center justify-between px-8 min-md:pl-14 pt-10 bg-gradient-to-r from-[#0558FE] to-[#A9CFFF] max-w-6xl mx-3 md:mx-auto rounded-2xl overflow-hidden'>
 
-        <div className='text-white'>
+        {isOwner ? (
+          <div className='text-white'>
+            <h2 className='text-3xl font-medium'>Add Another Car To Your Fleet!</h2>
+            <p className='mt-2'>Make more money from your car by renting it out!</p>
+            <p className='max-w-130'>We take care of insurence, driver verification and secure payments- so you can earn passive income, stress-free. </p>
+
+            <motion.button
+            whileHover={{scale: 1.05}}
+            whileTap={{scale: 0.95}}
+            className='px-6 py-2 bg-white hover:bg-slate-100 transition-all text-primary rounded-lg text-sm mt-4 cursor-pointer'>List your car</motion.button>
+        </div>
+        ) : (
+          <div className='text-white'>
             <h2 className='text-3xl font-medium'>Do You Own a Luxury Car?</h2>
             <p className='mt-2'>Monetize your vehicle effortlessly by listing it on  CarRental.</p>
             <p className='max-w-130'>We take care of insurence, driver verification and secure payments- so you can earn passive income, stress-free. </p>
@@ -20,6 +34,8 @@ const Banner = () => {
             whileTap={{scale: 0.95}}
             className='px-6 py-2 bg-white hover:bg-slate-100 transition-all text-primary rounded-lg text-sm mt-4 cursor-pointer'>List your car</motion.button>
         </div>
+        )}
+        
 
         <motion.img
         initial = {{opacity: 0, x: 50}}
